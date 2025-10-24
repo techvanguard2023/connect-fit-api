e2ca215fd23668ddad48bb6ea928a825f0ef7d1d
Tech Vanguard <suporte@techvanguard.com.br>
2025-10-23 22:38:58 -0300

    usuario de teste

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -24,7 +24,7 @@ class CustomerSeeder extends Seeder
 
         Customer::create([
             'user_type_id' => 1,
-            'stripe_customer_id' => '',	
+            'stripe_customer_id' => 'cus_THy7Mxatf0EVpE',	
             'name' => 'Robson Gomes Pedreira',
             'email' => 'masterdba6@gmail.com',
             'phone' => '21981321890',
